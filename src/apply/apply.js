3334a1276a3a2466f11fd9b929caf4abc8be72ae
Laura Badgley <l.badgleyy@gmail.com>
2019-02-06 10:36:09 -0800

    commit

--- src/apply/apply.js
+++ src/apply/apply.js
@@ -12,3 +12,27 @@ const applicantTwo = {
     power:"100% badass"
 };
 
+// initiate array for applicants
+let applicants = [];
+
+// push first applicant
+applicants.push(applicantOne);
+
+// store array in local storage
+const serializeOne = JSON.stringify(applicants);
+window.localStorage.setItem('applicants', serializeOne);
+
+// checking for existing applicants
+const jsonString = window.localStorage.getItem('applicants');
+// IMPORTANT TO SAVE NEW APPLICANTS WITHOUT OVERRIDING
+if(jsonString) {
+    applicants = JSON.parse(jsonString);
+}
+
+applicants.push(applicantTwo);
+
+const serializeTwo = JSON.stringify(applicants);
+window.localStorage.setItem('applicants', serializeTwo);
+
+const serializeThree = JSON.stringify('applicants');
+window.localStorage.setItem('applicants', serializeThree);
