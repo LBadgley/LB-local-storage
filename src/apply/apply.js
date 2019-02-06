48ea8e83621358f270a9470d0b5a32d5219580df
Laura Badgley <l.badgleyy@gmail.com>
2019-02-06 10:08:57 -0800

    initial commit

---
+++ src/apply/apply.js
@@ -0,0 +1,14 @@
+const applicantOne = {
+    name:"Tyrion",
+    lastName:"Lannister",
+    tvShow: "GOT",
+    power:"Drinking and knowledge"
+};
+
+const applicantTwo = {
+    name:"Arya",
+    lastName:"Stark",
+    tvShow:"GOT",
+    power:"100% badass"
+};
+
